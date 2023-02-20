062e282b599eb1ab8dc586eff67d20f1a43e8d06
Dahian <shaco-dark@outlook.com>
2023-02-20 18:58:08 -0300

    crud

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { request } from "express";
 import ProductManager from "./controllers/ProductManager.js";
 
-const product = new ProductManager
+const productManager = new ProductManager
 /*//! Crear productos
 product.addProduct("Titulo", "Description", 150, "Image", "adc120", 3);
 product.addProduct("Titulo1", "Description1", 230, "Image1", "adc121", 2);
@@ -23,40 +23,40 @@ app.use(express.urlencoded({extended: true}));
 
 app.get("/products", async (req, res) => {
     let limit = parseInt(req.query.limit);
-    let products = await product.getProducts();
-    limit > 0 ? res.send(products.slice(0, limit)) : res.send(products)
+    let products = await productManager.getProducts();
+    limit > 0 ? res.send(JSON.stringify(products.slice(0, limit))) : res.send(JSON.stringify(products))
 })
 
-app.get("/products/:pid", async (req, res) => {
-    let productId = parseInt(req.params.pid)
-    let products = await product.getProductsById(productId);
-    
-    res.send(products)
+app.get("/products/:id", async (req, res) => {
+    let productId = parseInt(req.params.id)
+    let productById = await productManager.getProductsById(productId);
+    res.send(JSON.stringify(productById))
+})
+
+app.post("/products", async (req, res) => {
+    let { title, description, price, thumbnail, code, stock } = req.body
+    await productManager.addProduct(title, description, price, thumbnail, code, stock)
+    res.send("Producto Agregado")
+})
+
+app.put("/products/:id", async (req, res) => {
+    let productId = parseInt(req.params.id);
+    let productUpdated = req.body
+    let productById = await productManager.getProductsById(productId);
+    if (productById && productUpdated) {
+        productManager.updateProduct(productId, productUpdated)
+        res.send(`Producto ${productId} updated`);
+    }else{
+        res.send("No se puede actualizar") //!Enviar error
+    }
+})
+
+app.delete("/products/:id", async (req, res) => {
+    let productId = parseInt(req.params.id)
+    await productManager.deleteProduct(productId);
+    res.send(`product ${productId} deleted`);
 })
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
 })
-
-
-/*
-app.get("/user/:id", (req, res) => {
-    console.log(req.params.id);
-    res.send("Bienvenido a mi pagina de Get User");
-})
-
-app.post("/user/:id", (req, res) => {
-    console.log(req.params.id);
-    res.send("Bienvenido a mi pagina de Post User");
-})
-
-app.put("/user/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    res.send("Bienvenido a mi pagina de Put User");
-})
-
-app.delete("/user/:id", (req, res) => {
-    console.log(req.params.id);
-    res.send("Bienvenido a mi pagina de Delete User");
-})*/
